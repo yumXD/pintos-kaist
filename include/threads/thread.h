0a437e13346a58aaa32acc1c52779a1b2386d6c7
yumXD <qkpw99@gmail.com>
2024-05-02 14:08:53 +0000

    Feat: thread 구조체에 일어나야 하는 시간 필드 추가

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -92,6 +92,7 @@ struct thread
 	enum thread_status status; /* Thread state. */
 	char name[16];			   /* Name (for debugging purposes). */
 	int priority;			   /* Priority. */
+	int64_t wakeup;			   // 깨어나야 하는 ticks 값
 
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem; /* List element. */
