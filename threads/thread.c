436b44326fdb59eb62f2b0f23bde13d80f378b35
yumXD <qkpw99@gmail.com>
2024-05-03 02:23:38 +0000

    Feat: thread_set_priority 함수 내 코드 추가 및 priority inversion 문제(donation) 해결완료
     만약 현재 진행중인 running thread의 priority 변경이 일어났을 때(init_priority 의 변경), donations리스트들에 있는 스레드들보다 priority가 높아지는 경우가 생길 수 있기 때문에 priority는 donations리스트 중 가장 높은 priority가 아니라 새로 바뀐 priority가 적용되게 수정

--- threads/thread.c
+++ threads/thread.c
@@ -409,7 +409,8 @@ void refresh_priority(void)
 /* Sets the current thread's priority to NEW_PRIORITY. */
 void thread_set_priority(int new_priority)
 {
-	thread_current()->priority = new_priority;
+	thread_current()->init_priority = new_priority;
+	refresh_priority();
 	thread_test_preemption();
 }
 
