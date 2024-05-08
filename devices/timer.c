7a34025040dab4bfbbee6c2c85f883e3b7dbdd55
yumXD <qkpw99@gmail.com>
2024-05-08 04:17:03 +0000

    Fix: alarm-simultaneous 간혈적 에러 해결..?

--- devices/timer.c
+++ devices/timer.c
@@ -96,7 +96,7 @@ void timer_sleep(int64_t ticks)
 	int64_t start = timer_ticks();
 
 	ASSERT(intr_get_level() == INTR_ON);
-	if (timer_elapsed(start) < ticks)
+	if (timer_elapsed(start) <= ticks)
 		thread_sleep(start + ticks);
 }
 
